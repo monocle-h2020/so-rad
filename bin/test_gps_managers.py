1d8dea99dade9b7970b40dcd654016d34df165ad
oco <oco@pml.ac.uk>
2019-08-22 15:00:34 +0100

    tested gps_test script

--- bin/test_gps_managers.py
+++ bin/test_gps_managers.py
@@ -11,13 +11,22 @@ import main_app
 import time
 import sys
 from thread_managers.gps_manager import GPSManager
+import RPi.GPIO as GPIO
 
-if __name__ == '__main__':
+
+def gpio_off():
+    GPIO.setmode(GPIO.BOARD)
+    pins = [11,12,13,15]  # FIXME: get from configs
+    GPIO.setup(pins, GPIO.OUT)
+    GPIO.output(pins, GPIO.LOW)
+    GPIO.cleanup()
+    print("done")
+
+def run_test():
     args = main_app.parse_args()
     conf = main_app.read_config(args.config_file)
     ports = list_ports.comports()
     gps = initialisation.gps_init(conf['GPS'], ports)
-
 
     gps_ports = [port for key, port in gps.items() if 'serial' in key]
     # Instantiate GPS monitoring threads
@@ -33,18 +42,23 @@ if __name__ == '__main__':
     time.sleep(0.1)
 
     # Start the GPS checker thread
-    #gps_checker_manager = GPSChecker(gps_managers)
-    #gps_checker_manager.start()
+    # gps_checker_manager = GPSChecker(gps_managers)
+    # gps_checker_manager.start()
 
     while True:
         try:
             for gpsman in gps_managers:
-                print(gpsman.serial_ports[0], gpsman.last_update, gpsman.lat, gpsman.lon, gpsman.speed, gpsman.fix)
-                time.sleep(0.05)
+                print(gpsman.serial_ports[0].port, gpsman.last_update, gpsman.lat, gpsman.lon, gpsman.speed, gpsman.fix)
+                time.sleep(0.2)
         except (KeyboardInterrupt, SystemExit):
             for gps_manager in gps_managers:
                 print("Stopping GPS manager thread")
                 del(gps_manager)
+            gpio_off()
             sys.exit(0)
         except Exception:
             raise
+
+
+if __name__ == '__main__':
+    run_test()
