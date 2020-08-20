ca899474b23527f715c0180fe00539e411956d51
stsi <stsi@pml.ac.uk>
2020-08-20 17:01:10 +0100

    Tested: correct TPR angles

--- bin/tests/test_TPR.py
+++ bin/tests/test_TPR.py
@@ -24,27 +24,40 @@ class ADXL345(object):
         """
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.accelerometer = adafruit_adxl34x.ADXL345(self.i2c)
-        #self.tilt = 0.0
+        self.tilt = None
         self.pitch = None
         self.roll = None
         self.updated = None
+        # the orientation of our sensor is not as written on the board, so here we adjust
+        # normally x = 0, y = 1, z = 2. Store this in the config file?
+        self.xindex = 1  # config
+        self.yindex = 2  # config
+        self.zindex = 0  # config
 
     def get_pitch_roll(self):
         '''Pitch and Roll from rotation around X and Y axes'''
-        x = self.accelerometer.acceleration[0]
-        y = self.accelerometer.acceleration[1]
-        z = self.accelerometer.acceleration[2]
-        #distance = math.sqrt((x * x) + (y * y))
-        x_axis_rotation = math.atan2(y, math.sqrt((x * x) + (z * z)) )
-        y_axis_rotation = -math.atan2(x, math.sqrt((y * y) + (z * z)) )
+        x = float(self.accelerometer.acceleration[self.xindex])
+        y = float(self.accelerometer.acceleration[self.yindex])
+        z = float(self.accelerometer.acceleration[self.zindex])
+
+        #x_axis_rotation = math.atan2(y, math.sqrt((x**2) + (z**2)) )  # x and y planes are exchangeable
+        #y_axis_rotation = -math.atan2(x, math.sqrt((y**2) + (z**2)) )
+
+        x_axis_rotation = math.atan( y / math.sqrt(x**2 + z**2) )  # this is the roll rotation, around the x (forward) axis. positive is to the right
+        y_axis_rotation = math.atan( x / math.sqrt(y**2 + z**2) )  # this is the pitch rotation, around the y (sideward) axis. positive is up.
+        if z != 0.0:
+            z_axis_rotation = math.atan( math.sqrt(x**2 + y**2) / z )  # Theta or Tilt rotation, between z (up) axis and gravity
+        else:
+            z_axis_rotation = 90.0
 
         self.pitch = math.degrees(x_axis_rotation)
         self.roll  = math.degrees(y_axis_rotation)
+        self.tilt  = - math.degrees(z_axis_rotation)
         self.updated = datetime.datetime.now()
-        return self.updated, self.pitch, self.roll
+        return self.updated, self.tilt, self.pitch, self.roll
 
 
-def main(config, duration):
+def main(config, duration, sleep):
     # get protocol from config
     protocol = "ADXL345"
     acc = ADXL345()
@@ -53,19 +66,21 @@ def main(config, duration):
 
     while time.time() < t1 + duration:
 
-        t, p, r = acc.get_pitch_roll()
-        print("{0} | Pitch: {1:2.5f} \t Roll: {2:2.5f}".format(t, p, r))
+        u, t, p, r = acc.get_pitch_roll()
+        print("{0} | Tilt: {1:2.2f} \t Pitch: {2:2.2f} \t Roll: {3:2.2f}".format(u, t, p, r))
 
-        time.sleep(0.1)
+        time.sleep(sleep)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--duration", default=3, type=int, help="Test duration in seconds (default 10s)")
+    parser.add_argument("-s", "--sleep", default = 0.1, type=float, help="Sleep time between samples (min ~0.01s, default 0.1s)")
     parser.add_argument("-c", "--config", required=True, help="Path to config file")
     args = parser.parse_args()
 
     # read TPR usage and protocol from config file
+    # use the initialisation module to get the right TPR class
 
-    main(args.config, args.duration)
+    main(args.config, args.duration, args.sleep)
 
