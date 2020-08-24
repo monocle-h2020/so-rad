2b02c6ee9644bc95da771cf534f7b978920bd6ca
stsi <stsi@pml.ac.uk>
2020-08-24 18:17:35 +0100

    first commit of working tpr thread monitor, needs filtering for out-of-scope readings

--- bin/initialisation.py
+++ bin/initialisation.py
@@ -20,6 +20,7 @@ import logging
 from functions import gps_functions as gps_func
 from thread_managers import radiometer_manager
 from thread_managers import battery_manager
+from thread_managers import tpr_manager
 from functions import db_functions
 log = logging.getLogger()   # report to root logger
 
@@ -129,10 +130,38 @@ def battery_init(battery_config, ports):
     assert battery['interface'] in ['victron',]
 
     # Return the battery configuration dict and relevant manager class
-    if battery['interface'] == 'victron':
+    if battery['interface'].lower() == 'victron':
         bat_manager = battery_manager.VictronManager(battery, ports)
 
     return battery, bat_manager
+
+
+def tpr_init(tpr_config):
+    """
+    Read Tilt/Pitch/Roll monitoring config settings and initialise TPR connection and monitor
+
+    : tpr_config is the [TPR] section in the config file
+    : tpr is a dictionary containing the configuration 
+    """
+    tpr = {}
+    # Get all the TPR variables from the config file
+    tpr['used'] = tpr_config.getboolean('use_tpr')
+    tpr['interface'] = tpr_config.get('protocol')
+    tpr['sampling_time'] = tpr_config.getint('sampling_time')
+    tpr['xindex'] = tpr_config.getint('xindex')
+    tpr['yindex'] = tpr_config.getint('yindex')
+    tpr['zindex'] = tpr_config.getint('zindex')
+
+    if not tpr['used']:
+        return tpr, None
+
+    assert tpr['interface'].lower() in ['ada_adxl345', ]
+
+    # Return the battery configuration dict and initialise relevant manager class
+    if tpr['interface'].lower() == 'ada_adxl345':
+        tpr_man = tpr_manager.Ada_adxl345(tpr)
+
+    return tpr, tpr_man
 
 
 def gps_init(gps_config, ports):
@@ -232,6 +261,7 @@ def gps_init(gps_config, ports):
     # Return the GPS dict
     return gps
 
+
 def gps_rtk_init(gps_config):
     """read gps configuration for rtk gps. Any other initialisation should also be called here"""
 
@@ -266,16 +296,15 @@ def gps_rtk_init(gps_config):
         # Get the known GPS ports from the config file
         log.info("Defaulting to GPS port settings in config file")
         gps['port1'] = gps_config.get('port1_default')
-   
+
     time.sleep(1)
-    
-   
 
     # Create serial objects for both the GPS sensor ports using variables from the config file
     gps['serial1'] = serial.Serial(port=gps['port1'], baudrate=gps['baud1'], timeout=0.5, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, xonxoff=False)
-   
+
     # Return the GPS dict
     return gps
+
 
 def rad_init(rad_config, ports):
     """read radiometer configuration. Any other initialisation should also be called here"""
--- bin/tests/test_TPR.py
+++ bin/tests/test_TPR.py
@@ -4,83 +4,57 @@
 Simple test for Tilt Pitch Roll (TPR) sensor (ADXL345)
 """
 
+import sys
+import os
 import time
-import board
-import busio
-import adafruit_adxl34x
-import numpy as np
-import argparse
-import datetime
-import math
+import inspect
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
+import serial.tools.list_ports as list_ports
+from initialisation import tpr_init
+from main_app import parse_args, read_config
+
+test_duration_single_reads = 1  # seconds
+test_duration_monitor_average = 30  # seconds
 
 
-class ADXL345(object):
-    '''
-    ADXL345 via adafruit_adxl34x library giving Tilt/Pitch/Roll
-    '''
-    def __init__(self):
-        """
-        Set up
-        """
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.accelerometer = adafruit_adxl34x.ADXL345(self.i2c)
-        self.tilt = None
-        self.pitch = None
-        self.roll = None
-        self.updated = None
-        # the orientation of our sensor is not as written on the board, so here we adjust
-        # normally x = 0, y = 1, z = 2. Store this in the config file?
-        self.xindex = 1  # config
-        self.yindex = 2  # config
-        self.zindex = 0  # config
+def main(conf):
+    print("start test, initialising")
+    tpr, tpr_manager = tpr_init(conf['TPR'])
 
-    def get_pitch_roll(self):
-        '''Pitch and Roll from rotation around X and Y axes'''
-        x = float(self.accelerometer.acceleration[self.xindex])
-        y = float(self.accelerometer.acceleration[self.yindex])
-        z = float(self.accelerometer.acceleration[self.zindex])
-
-        #x_axis_rotation = math.atan2(y, math.sqrt((x**2) + (z**2)) )  # x and y planes are exchangeable
-        #y_axis_rotation = -math.atan2(x, math.sqrt((y**2) + (z**2)) )
-
-        x_axis_rotation = math.atan( y / math.sqrt(x**2 + z**2) )  # this is the roll rotation, around the x (forward) axis. positive is to the right
-        y_axis_rotation = math.atan( x / math.sqrt(y**2 + z**2) )  # this is the pitch rotation, around the y (sideward) axis. positive is up.
-        if z != 0.0:
-            z_axis_rotation = math.atan( math.sqrt(x**2 + y**2) / z )  # Theta or Tilt rotation, between z (up) axis and gravity
-        else:
-            z_axis_rotation = 90.0
-
-        self.pitch = math.degrees(x_axis_rotation)
-        self.roll  = math.degrees(y_axis_rotation)
-        self.tilt  = - math.degrees(z_axis_rotation)
-        self.updated = datetime.datetime.now()
-        return self.updated, self.tilt, self.pitch, self.roll
-
-
-def main(config, duration, sleep):
+    print("read data for {0} second".format(test_duration_single_reads))
     # get protocol from config
-    protocol = "ADXL345"
-    acc = ADXL345()
-
     t1 = time.time()
+    while time.time() < t1 + test_duration_single_reads:
 
-    while time.time() < t1 + duration:
-
-        u, t, p, r = acc.get_pitch_roll()
+        u, t, p, r = tpr_manager.update_pitch_roll_single()
         print("{0} | Tilt: {1:2.2f} \t Pitch: {2:2.2f} \t Roll: {3:2.2f}".format(u, t, p, r))
 
-        time.sleep(sleep)
+        time.sleep(0.01)
 
+
+    print("\n\nrun monitor for {0} seconds".format(test_duration_monitor_average))
+    tpr_manager.start()
+    t1 = time.time()
+    while time.time() < t1 + test_duration_monitor_average:
+        if None in [tpr_manager.avg_updated, tpr_manager.tilt_avg, tpr_manager.tilt_std,
+                      tpr_manager.pitch_avg, tpr_manager.pitch_std,
+                      tpr_manager.roll_avg, tpr_manager.roll_std]:
+            print("Waiting for average. Buffer length: {0}, oldest record: {1}".format(len(tpr_manager.buffer_tilt), tpr_manager.buffer_time[0]))
+        else:
+            print("{0} | Tilt: {1:2.2f} ({2:2.2f} \t Pitch: {3:2.2f} ({4:2.2f}) \t Roll: {5:2.2f} ({6:2.2f})"\
+                     .format(tpr_manager.avg_updated, tpr_manager.tilt_avg, tpr_manager.tilt_std,
+                      tpr_manager.pitch_avg, tpr_manager.pitch_std,
+                      tpr_manager.roll_avg, tpr_manager.roll_std))
+        time.sleep(1)
+
+    print("finished test, stopping monitor")
+    tpr_manager.stop()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--duration", default=3, type=int, help="Test duration in seconds (default 10s)")
-    parser.add_argument("-s", "--sleep", default = 0.1, type=float, help="Sleep time between samples (min ~0.01s, default 0.1s)")
-    parser.add_argument("-c", "--config", required=True, help="Path to config file")
-    args = parser.parse_args()
-
+    args = parse_args()
+    conf = read_config(args.config_file)
     # read TPR usage and protocol from config file
     # use the initialisation module to get the right TPR class
 
-    main(args.config, args.duration, args.sleep)
+    main(conf)
 
