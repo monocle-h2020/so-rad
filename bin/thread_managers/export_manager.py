785df231fb00350ff53e89b9f2e47d5f77483f07
Stefan <stsi@pml.ac.uk>
2025-09-07 18:05:09 +0100

    speed up export_manager exit

--- bin/thread_managers/export_manager.py
+++ bin/thread_managers/export_manager.py
@@ -46,9 +46,13 @@ class ParseExportManager(object):
         self.n_total = None
         self.n_not_inserted = None
         self.all_not_inserted = None
-        self.last_db_check = datetime.datetime.now() - datetime.timedelta(seconds=self.upload_interval)  # prime timer
-        self.last_status_update = datetime.datetime.now() - datetime.timedelta(seconds=self.status_update_interval) # prime timer
-        self.last_data_export = datetime.datetime.now() - datetime.timedelta(seconds=self.upload_interval)  # prime timer
+
+        # prime all timers
+        self.last_db_check = datetime.datetime.now() - datetime.timedelta(seconds=self.upload_interval)
+        self.last_status_update = datetime.datetime.now() - datetime.timedelta(seconds=self.status_update_interval)
+        self.last_data_export = datetime.datetime.now() - datetime.timedelta(seconds=self.upload_interval)
+        self.last_connectivity_check_time = datetime.datetime.now() - datetime.timedelta(seconds=self.connection_retry_interval)
+        self.last_connectivity_check_result = False
 
     def update_values(self):
         """
@@ -95,6 +99,9 @@ class ParseExportManager(object):
         log.info("Starting Export manager thread")
         while not self.stop_monitor:
 
+            connected_internet = False
+            connected_parse = False
+
             # check how many samples need uploading, upload a batch and check again
             if self.last_db_check + datetime.timedelta(seconds=self.upload_interval) < datetime.datetime.now():
                 # check local db
@@ -105,16 +112,29 @@ class ParseExportManager(object):
                     log.info(f"{n_not_inserted} samples pending upload")
                     rf.store(redis_client, 'samples_pending_upload', self.n_not_inserted, expires=30)
 
-            if not check_internet():
-                log.debug(f"Internet connection timed out. Retry in 300s")
-                rf.store(redis_client, 'upload_status', 'no_connection', expires=30)
-                time.sleep(self.connection_retry_interval)
+            if (not self.last_connectivity_check_result) and \
+               (self.last_connectivity_check_time + datetime.timedelta(seconds=self.connection_retry_interval) < datetime.datetime.now()):
+                # we didn't have a connection last time, and have waited the requisite interval before retrying
+                if not check_internet():
+                    log.debug(f"Internet connection timed out. Retry in 300s")
+                    rf.store(redis_client, 'upload_status', 'no_internet_connection', expires=30)
+                    self.last_connectivity_check_result = False
+                    self.last_connectivity_check_time = datetime.datetime.now()
+                    time.sleep(self.sleep_interval)
+                    continue
+                else:
+                    self.last_connectivity_check_result = True
+                    self.last_connectivity_check_time = datetime.datetime.now()
+            else:
+                time.sleep(self.sleep_interval)
                 continue
 
             # system status upload
             if self.last_status_update + datetime.timedelta(seconds=self.status_update_interval) < datetime.datetime.now():
                 export_result = None
                 if check_remote_data_store(self.export_dict)[0]:
+                    self.last_connectivity_check_result = True
+                    self.last_connectivity_check_time = datetime.datetime.now()
                     export_result, resultcode = update_status_parse_server(self.export_dict, self.db_dict)
                     sucorfail = {True: 'succeeded', False:'failed'}[export_result]
                     log.info(f"Instrument status update on remote server {sucorfail}")
@@ -123,9 +143,14 @@ class ParseExportManager(object):
                     self.last_status_update = datetime.datetime.now()
                 else:
                     log.debug(f"No connection to remote server to update instrument status. Retry in 300s")
-                    rf.store(redis_client, 'upload_status', 'no_connection', expires=30)
-                    time.sleep(self.connection_retry_interval)
+                    rf.store(redis_client, 'upload_status', 'no_connection_to_server', expires=30)
+                    self.last_connectivity_check_result = False
+                    self.last_connectivity_check_time = datetime.datetime.now()
+                    time.sleep(self.sleep_interval)
                     continue
+            else:
+                time.sleep(self.sleep_interval)
+                continue
 
             # data upload
             if (self.n_not_inserted > 0) and \
@@ -133,6 +158,8 @@ class ParseExportManager(object):
                   (self.last_data_export + datetime.timedelta(seconds=self.upload_interval) < datetime.datetime.now()):
 
                 if check_remote_data_store(self.export_dict)[0]:
+                    self.last_connectivity_check_result = True
+                    self.last_connectivity_check_time = datetime.datetime.now()
                     while (n_not_inserted > 0) and (export_result):
                         # upload data until no more samples remain or an upload fails.
                         log.debug("Uploading latest 10 samples ({n_not_inserted} pending)")
@@ -147,7 +174,9 @@ class ParseExportManager(object):
                 else:
                     log.debug(f"No connection to remote server. Retry in 300s")
                     rf.store(redis_client, 'upload_status', 'no_connection', expires=30)
-                    time.sleep(self.connection_retry_interval)
+                    self.last_connectivity_check_result = False
+                    self.last_connectivity_check_time = datetime.datetime.now()
+                    time.sleep(self.sleep_interval)
 
             else:
                 rf.store(redis_client, 'upload_status', 'idle', expires=30)
