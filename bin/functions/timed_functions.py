ee56ea3cc22b6bdb05c610dfdca2f9426c433b38
Stefan <stsi@pml.ac.uk>
2025-10-13 16:23:51 +0000

    timedatectl adjustment

--- bin/functions/timed_functions.py
+++ bin/functions/timed_functions.py
@@ -15,10 +15,11 @@ import sys
 import logging
 import datetime
 from functions import redis_functions as rf
+from numpy import sign
+import subprocess
 
 redis_client = rf.init()
 log = logging.getLogger('timed')
-#log.setLevel('DEBUG')
 
 def sync_clocks():
     """
@@ -44,5 +45,40 @@ def sync_clocks():
        sudo hwclock -s
 
     """
-    latest_gps, latest_gps_updated, fresh = rf.retrieve(client, key, freshness=5)
-    log.info(f"Latest gps: {latest_gps} updated at {latest_gps_updated}. Recent: {fresh}")
+    latest_gps, latest_gps_updated, expired = rf.retrieve(redis_client, 'gps_manager', freshness=5)
+
+    # Check that system timezone is UTC or Etc/UTC
+    timezone_name = datetime.datetime.now(datetime.datetime.now().astimezone().tzinfo).tzname()
+    if 'UTC' not in timezone_name:
+        log.error(f"Not implemented: local timezone is {timezone_name}, expecting 'UTC'")
+        return
+
+    if expired:
+        log.debug(f"Latest gps update is from {latest_gps_updated}. No action taken.")
+        return
+    elif not expired:
+        log.debug(f"Latest gps timestamp: {latest_gps['datetime']} updated at {latest_gps_updated}. Proceed.")
+
+    # get sync offset in seconds
+    delta = latest_gps['last_update'] - latest_gps['datetime']  # system time vs gps time at time of update, +ve means system is ahead
+    delta_s = delta.total_seconds()
+
+    if abs(delta_s) > 3.0:
+        if sign(delta_s) > 0:
+            log.info(f"System clock is {abs(delta_s)} ahead of GPS. Synchronizing.")
+        else:
+            log.info(f"System clock is {abs(delta_s)} behind GPS. Synchronizing")
+
+        # adjust the hwclock
+        target_time = datetime.datetime.now() - datetime.timedelta(seconds=delta_s)
+        target_str = datetime.datetime.strftime(target_time, "%Y-%m-%d %H:%M:%S")
+        log.info(f"Set system time to {target_str}")
+        cmd = f"sudo timedatectl --no-ask-password set-ntp false; sudo timedatectl --no-ask-password set-time '{target_str}'; sudo timedatectl --no-ask-password set-ntp true"
+        proc = subprocess.run(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        log.debug(proc.stdout)
+        log.debug(proc.stderr)
+        log.info(f"System time adjusted by {delta_s} s")
+
+    else:
+        log.debug(f"System clock is within {abs(delta_s)} from GPS.")
+
